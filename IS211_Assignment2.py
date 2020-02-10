6dd4ce876c1a61be8f89ebefd404e3a89bb10d2e
Gitprincesskerry <60557711+Gitprincesskerry@users.noreply.github.com>
2020-02-10 18:52:02 -0500

    Update IS211_Assignment2.py

--- IS211_Assignment2.py
+++ IS211_Assignment2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # This is Kerry Rainford's Week 2 Assignment
 
 def downloadData(url=str):
@@ -9,7 +11,24 @@ def downloadData(url=str):
 # do I need to tell the function where to put the file?
 
 import datetime
-
+import time
 def processData():
     """Takes the contents of the file as the first parameter, process the file line by line, and returns
     a dictionary. The dictionary maps a person's ID to a tuple (name, birthday)"""
+
+def displayPerson(id=int, personData={}):
+    """This function prints the name and birthday of a given user identified by the input ID"""
+
+import logging
+
+logger = logging.getLogger("assignment2.xls")
+
+csvLogger = logging.getLogger("assignment2.csv")
+csvLogger.debug("Trying")
+csvLogger.warning("File")
+csvLogger.error("File unexpected")
+csvLogger.critical("File too large")
+
+logger.exception("Error reading file")
+logger.error("Error reading file")
+logger.log(logging.error, "Error reading file '%s' at offset %d", assignment2.csv, offset, exc_info=1)
